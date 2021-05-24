838891ee8bc8c7d0b31c62e1fa3a7c9cec3bbd65
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2021-05-24 12:50:10 -0400

    add url comentar

--- meublog/urls.py
+++ meublog/urls.py
@@ -5,5 +5,6 @@ app_name = 'meublog'
 urlpatterns = [
     path('', views.ListarPostView.as_view(), name='listar_posts'),
     path('<int:ano>/<int:mes>/<int:dia>/<slug:slug>/', views.DetalharPostView.as_view(), name='detalhe'),
-    path('sharepost/<int:pk>', views.FormContatoView.as_view(), name='share_post')
+    path('sharepost/<int:pk>', views.FormContatoView.as_view(), name='share_post'),
+    path('comentar/<int:pk>', views.FormComentarioView.as_view(), name='comentar')
 ]
