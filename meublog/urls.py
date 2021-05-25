ce0459c5ca858091b126068f900a23629023381f
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2021-05-24 22:48:50 -0400

    add link for listcomentarios

--- meublog/urls.py
+++ meublog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.ListarPostView.as_view(), name='listar_posts'),
     path('<int:ano>/<int:mes>/<int:dia>/<slug:slug>/', views.DetalharPostView.as_view(), name='detalhe'),
     path('sharepost/<int:pk>', views.FormContatoView.as_view(), name='share_post'),
-    path('comentar/<int:pk>', views.FormComentarioView.as_view(), name='comentar')
+    path('comentar/<int:pk>', views.FormComentarioView.as_view(), name='comentar'),
+    path('comentarios/<int:pk>', views.ListarComentarioView.as_view(), name='comentarios'),
 ]
