3d9e616e6bc816188d2fb3b2678bc0ed84d5a13c
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2021-06-01 23:41:11 -0400

    update urls

--- meublog/urls.py
+++ meublog/urls.py
@@ -4,8 +4,7 @@ from . import views
 app_name = 'meublog'
 urlpatterns = [
     path('', views.ListarPostView.as_view(), name='listar_posts'),
-    path('<int:ano>/<int:mes>/<int:dia>/<slug:slug>/', views.DetalharPostView.as_view(), name='detalhe'),
+    path('<slug:slug>/', views.DetalharPostView.as_view(), name='detalhe'),
     path('sharepost/<int:pk>', views.FormContatoView.as_view(), name='share_post'),
-    path('comentar/<int:pk>', views.FormComentarioView.as_view(), name='comentar'),
-    path('comentarios/<int:pk>', views.ListarComentarioView.as_view(), name='comentarios'),
+    path('comentar/<int:pk>', views.ComentarioView.as_view(), name='comentar_post')
 ]
