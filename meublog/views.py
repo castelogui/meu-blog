d9210ed4cc4fcf128a8387f4c70129bc51ae75be
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2021-06-01 23:39:57 -0400

    add correct view for comments

--- meublog/views.py
+++ meublog/views.py
@@ -1,9 +1,10 @@
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.contrib import messages
-from django.views.generic import ListView, DetailView, FormView
+from django.views.generic import ListView, DetailView, FormView, CreateView
 
-from .forms import EmailPost, ComentarioPost
-from .models import Comentario, Post
+from .forms import EmailPost, ComentarioModelForm
+from .models import Post, Comentario
 
 
 class ListarPostView(ListView):
@@ -13,15 +14,20 @@ class ListarPostView(ListView):
     template_name = "meublog/post/listarposts.html"
 
 
-class ListarComentarioView(ListView):
-    queryset = Comentario.publicado.all()
-    context_object_name = 'comentarios'
-    template_name = "meublog/post/listarcomentarios.html"
-
-
 class DetalharPostView(DetailView):
     template_name = "meublog/post/detalharpost.html"
     model = Post
+
+    def _get_comment(self, id_post):
+        try:
+            return Comentario.objects.filter(post_id=id_post, status=True)
+        except Comentario.DoesNotExixt:
+            raise Exception
+
+    def get_context_data(self, **kwargs):
+        context = super(DetalharPostView, self).get_context_data(**kwargs)
+        context['coments'] = self._get_comment(self.object.id)
+        return context
 
 
 class FormContatoView(FormView):
@@ -46,40 +52,31 @@ class FormContatoView(FormView):
         form.enviar_email(meupost)
         messages.success(self.request, f"Post {meupost.titulo} enviado com sucesso!")
 
-        return super(FormContatoView,self).form_valid(form, *args, **kwargs)
+        return super(FormContatoView, self).form_valid(form, *args, **kwargs)
 
     def form_invalid(self, form, *args, **kwargs):
         meupost = self.get_context_data()['post']
         messages.error(self.request, f"Post {meupost.titulo} Não enviado!")
 
-        return super(FormContatoView,self).form_invalid(form, *args, **kwargs)
+        return super(FormContatoView, self).form_invalid(form, *args, **kwargs)
 
 
-class FormComentarioView(FormView):
-    template_name = 'meublog/post/comentar.html'
-    form_class = ComentarioPost
-    success_url = reverse_lazy('meublog:listar_posts')
+class ComentarioView(CreateView):
+    template_name = 'meublog/post/comentarios.html'
+    form_class = ComentarioModelForm
 
     def get_post(self, id_post):
         try:
             return Post.publicado.get(pk=id_post)
         except Post.DoesNotExist:
-            messages.error(self.request, 'O post não existe!')
-            reverse_lazy('meublog:listar_posts')
+            raise Exception
 
     def get_context_data(self, **kwargs):
-        context = super(FormComentarioView, self).get_context_data(**kwargs)
+        context = super(ComentarioView, self).get_context_data(**kwargs)
         context['post'] = self.get_post(self.kwargs['pk'])
         return context
 
     def form_valid(self, form, *args, **kwargs):
-        meupost = self.get_context_data()['post']
-        form.comentar(meupost)
-
-        return super(FormComentarioView,self).form_valid(form, *args, **kwargs)
-
-    def form_invalid(self, form, *args, **kwargs):
-        meupost = self.get_context_data()['post']
-        messages.error(self.request, f"Post {meupost.titulo} Não enviado!")
-
-        return super(FormComentarioView,self).form_invalid(form, *args, **kwargs)
+        post = self.get_post(self.kwargs['pk'])
+        form.comentar(post)
+        return redirect('meublog:detalhe', post.slug)
