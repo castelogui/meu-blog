b1d0a264d406b6191781720096b09fccf732a7a7
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2021-06-03 20:49:59 -0400

    add urls for control user

--- meublog/urls.py
+++ meublog/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.ListarPostView.as_view(), name='listar_posts'),
     path('<slug:slug>/', views.DetalharPostView.as_view(), name='detalhe'),
     path('sharepost/<int:pk>', views.FormContatoView.as_view(), name='share_post'),
-    path('comentar/<int:pk>', views.ComentarioView.as_view(), name='comentar_post')
+    path('comentar/<int:pk>', views.ComentarioView.as_view(), name='comentar_post'),
+    path('cadastrousuario', views.CadUsuarioView.as_view(), name='cadastrouser'),
+    path('login', views.LoginUsuarioView.as_view(), name='loginuser'),
+    path('logout', views.LogoutUsuarioView.as_view(), name='logoutuser'),
 ]
